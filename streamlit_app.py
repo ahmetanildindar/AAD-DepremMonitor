b38091acf58fcb48bccd4a67792c493a6eff6aa8
Ahmet Anil Dindar <ahmetanildindar@gmail.com>
2023-08-23 15:29:15 +0300

    foilum hatası

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 import datetime as dt
 
-import folium
+# import folium
 from streamlit_folium import st_folium
 #
 #
@@ -120,15 +120,15 @@ with tab_analiz :
                           
                 st.map( data = onceki_depremler_secili_df , latitude="Latitude", longitude = "Longitude" , size = "Magnitude")
                     
-with tab_test : 
-    # center on Liberty Bell, add marker
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-    folium.Marker(
-        [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-    ).add_to(m)
+# with tab_test : 
+#     # center on Liberty Bell, add marker
+#     m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+#     folium.Marker(
+#         [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+#     ).add_to(m)
 
-    # call to render Folium map in Streamlit, but don't get any data back
-    # from the map (so that it won't rerun the app when the user interacts)
-    st_folium(m, width=725, returned_objects=[])
+#     # call to render Folium map in Streamlit, but don't get any data back
+#     # from the map (so that it won't rerun the app when the user interacts)
+#     st_folium(m, width=725, returned_objects=[])
 
 # %%
