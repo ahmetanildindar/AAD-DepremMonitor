c2ed796853f06a7b91fdb744276cc7dd22a30f9d
Ahmet Anil Dindar <ahmetanildindar@gmail.com>
2023-10-02 23:28:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ with tab_bakis :
 
     onceki_depremler_df = kayitli_depremler( ) ;
 
-    onceki_depremler_df["Date"] = pd.to_datetime( onceki_depremler_df["Date"] , dayfirst = True ).dt.date
+    # onceki_depremler_df["Date"] = pd.to_datetime( onceki_depremler_df["Date"] , dayfirst = True ).dt.date
 
     metin = f"Şu anda bu sitede önceden kaydedilmiş **{len( onceki_depremler_df)}** adet deprem bulunmaktadır. En büyük deprem **{onceki_depremler_df[ onceki_depremler_df.Magnitude == onceki_depremler_df.Magnitude.max() ].iloc[0].Type} {onceki_depremler_df.Magnitude.max()}**, **{onceki_depremler_df[ onceki_depremler_df.Magnitude == onceki_depremler_df.Magnitude.max() ].iloc[0].Date}** tarihinde **{onceki_depremler_df[ onceki_depremler_df.Magnitude == onceki_depremler_df.Magnitude.max() ].iloc[0].Location}** konumunda gözlenmiştir."
 
